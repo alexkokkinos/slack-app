98136e2bd1244e22ad973129fa1c60371badc0ee
alexkokkinos <kokkinos.alex@gmail.com>
2021-11-23 01:49:21 -0500

    Please work in Heroku

--- app.py
+++ app.py
@@ -203,3 +203,14 @@ Set your location and other preferences in the <slack://app?team={body['team_id'
 
 if __name__ == "__main__":
   app.start(port=int(os.environ.get("PORT", 3000)))
+
+# Production
+from flask import Flask, request
+from slack_bolt.adapter.flask import SlackRequestHandler
+
+flask_app = Flask(__name__)
+handler = SlackRequestHandler(app)
+
+@flask_app.route("/slack/events", methods=["POST"])
+def slack_events():
+    return handler.handle(request)
