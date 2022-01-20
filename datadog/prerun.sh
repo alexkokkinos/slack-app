b0d33a79622e00afdbfa1acc85b9d99d4fa7367b
alexkokkinos <kokkinos.alex@gmail.com>
2022-01-20 13:33:07 -0500

    Added postgres prerun workaround

--- datadog/prerun.sh
+++ datadog/prerun.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 
 # Update the Postgres configuration from above using the Heroku application environment variable
-if [ -n "$DATABASE_URL" ]; then
-  POSTGREGEX='^postgres://([^:]+):([^@]+)@([^:]+):([^/]+)/(.*)$'
-  if [[ $DATABASE_URL =~ $POSTGREGEX ]]; then
-    sed -i "s/<YOUR HOSTNAME>/${BASH_REMATCH[3]}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
-    sed -i "s/<YOUR USERNAME>/${BASH_REMATCH[1]}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
-    sed -i "s/<YOUR PASSWORD>/${BASH_REMATCH[2]}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
-    sed -i "s/<YOUR PORT>/${BASH_REMATCH[4]}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
-    sed -i "s/<YOUR DBNAME>/${BASH_REMATCH[5]}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
-  fi
-  else
-  export THISWILLNEVERWORK="TESTING"
-fi
+sed -i "s/<YOUR HOSTNAME>/${DATABASE_HOST}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
+sed -i "s/<YOUR USERNAME>/${DATABASE_USER}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
+sed -i "s/<YOUR PASSWORD>/${DATABASE_PASSWORD}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
+sed -i "s/<YOUR PORT>/${DATABASE_PORT}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
+sed -i "s/<YOUR DBNAME>/$DATABASE_DBNAME/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
