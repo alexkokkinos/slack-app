b6561bac15de56bd6669f34c5d9ef443cb366928
alexkokkinos <kokkinos.alex@gmail.com>
2022-01-20 13:38:50 -0500

    Fixed prerun

--- datadog/prerun.sh
+++ datadog/prerun.sh
@@ -5,4 +5,4 @@ sed -i "s/<YOUR HOSTNAME>/${DATABASE_HOST}/" "$DD_CONF_DIR/conf.d/postgres.d/con
 sed -i "s/<YOUR USERNAME>/${DATABASE_USER}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
 sed -i "s/<YOUR PASSWORD>/${DATABASE_PASSWORD}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
 sed -i "s/<YOUR PORT>/${DATABASE_PORT}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
-sed -i "s/<YOUR DBNAME>/$DATABASE_DBNAME/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
+sed -i "s/<YOUR DBNAME>/${DATABASE_DBNAME}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
