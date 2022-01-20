55e92677946a6bcb2bcdafa5f7b20dd32f1b87f1
alexkokkinos <kokkinos.alex@gmail.com>
2022-01-20 14:33:53 -0500

    Fix prerun

--- datadog/prerun.sh
+++ datadog/prerun.sh
@@ -2,7 +2,7 @@
 
 # Update the Postgres configuration from above using the Heroku application environment variable
 sed -i "s/<YOUR HOSTNAME>/${DATABASE_HOST}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
-sed -i "s/<YOUR USERNAME>/$DATABASE_USER}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
+sed -i "s/<YOUR USERNAME>/${DATABASE_USER}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
 sed -i "s/<YOUR PASSWORD>/${DATABASE_PASSWORD}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
 sed -i "s/<YOUR PORT>/${DATABASE_PORT}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
 sed -i "s/<YOUR DBNAME>/${DATABASE_DBNAME}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
