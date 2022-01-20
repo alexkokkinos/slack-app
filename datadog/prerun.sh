d5c174d06a938d0cb5c1f51771b2dfcc91158905
alexkokkinos <kokkinos.alex@gmail.com>
2022-01-20 12:15:51 -0500

    Debugging

--- datadog/prerun.sh
+++ datadog/prerun.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-export PROVES_PRERUN_RAN="TESTING"
-
 # Update the Postgres configuration from above using the Heroku application environment variable
 if [ -n "$DATABASE_URL" ]; then
+  export WEGOTTHISFAR="Heroku"
   POSTGREGEX='^postgres://([^:]+):([^@]+)@([^:]+):([^/]+)/(.*)$'
   if [[ $DATABASE_URL =~ $POSTGREGEX ]]; then
+    export WHYNOWORK="Heroku"
     sed -i "s/<YOUR HOSTNAME>/${BASH_REMATCH[3]}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
     sed -i "s/<YOUR USERNAME>/${BASH_REMATCH[1]}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
     sed -i "s/<YOUR PASSWORD>/${BASH_REMATCH[2]}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
